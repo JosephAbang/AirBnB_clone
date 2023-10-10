df5ebc2c3ec218df05c5c5368ea19a1fe7cbf41f
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-10-10 09:46:00 +0000

    Add initializer and update dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -11,9 +11,11 @@ from datetime import datetime
 class BaseModel:
     """base class of all our models"""
 
-    id = str(uuid.uuid4())
-    created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-    updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+    def __init__(self):
+        """Initializes public class attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """Method defines str representation of instance"""
@@ -21,9 +23,12 @@ class BaseModel:
 
     def save(self):
         """Method updates the public instance attribute `updated_at`"""
-        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__"""
-        self.__dict__["__class__"] = self.__class__.__name__
-        return self.__dict__
+        obj_dict = self.__dict__.copy()
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        obj_dict["__class__"] = self.__class__.__name__
+        return obj_dict
