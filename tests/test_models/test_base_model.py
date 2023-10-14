9d7ba9537f0bd9380a5b6f834cef5889c11eaa21
JosephAbang <abangjosef@gmail.com>
2023-10-14 12:58:15 +0100

    Pycodestyle error fic for tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,13 +8,12 @@ from datetime import datetime as dt
 class TestBaseModel(unittest.TestCase):
     def test_Updated_at_type(self):
         assertEqual(type(BM.updated_at), type(dt.now()))
+
     def test_created_at_type(self):
         assertEqual(type(BM.created_at), type(dt.now()))
+
     def test_id_type(self):
         assertEqual(type(BM.id, str))
-
-    
-
 
 
 if __name__ == "__main__":
