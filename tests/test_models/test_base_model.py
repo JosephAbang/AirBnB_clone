c41525950b0ac8938c0d86bdb35efe5afd94b6b8
JosephAbang <abangjosef@gmail.com>
2023-10-14 19:45:02 +0100

    Use disscover tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,19 +1,22 @@
-#!/ussr/bin/python3
+#!/usr/bin/python3
 
 import unittest
-from models.base_model import BaseModel as BM
 from datetime import datetime as dt
+from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
     def test_Updated_at_type(self):
-        assertEqual(type(BM.updated_at), type(dt.now()))
+        b = BaseModel()
+        self.assertEqual(type(b.updated_at), type(dt.now()))
 
     def test_created_at_type(self):
-        assertEqual(type(BM.created_at), type(dt.now()))
+        b = BaseModel()
+        self.assertEqual(type(b.created_at), type(dt.now()))
 
     def test_id_type(self):
-        assertEqual(type(BM.id, str))
+        b = BaseModel()
+        self.assertEqual(type(b.id), str)
 
 
 if __name__ == "__main__":
