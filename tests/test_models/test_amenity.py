882d84fa611401d421f456055ce795004347e78e
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-10-15 00:37:03 +0000

    Add test for amenity, place, review
    Created unittest for class Amenity, Place, Review to be able to test our
    classes effectively

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+TestFile for Amenity class
+"""
+
+import unittest
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """Class defines Test file for Amenity
+    Inherits from BaseModel
+    """
+
+    def test_name(self):
+        obj = Amenity()
+        obj.name = "Wayne Dave"
+        self.assertEqual(type(obj.name), str)
+
+
+if __name__ == "__main__":
+    unittest.main()
