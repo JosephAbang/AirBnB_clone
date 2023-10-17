ff42f038e47d8f7ade9a79766db9db85acc8501f
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-10-17 12:09:40 +0000

    Modify test_name

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -17,7 +17,7 @@ class TestReview(unittest.TestCase):
     def test_name(self):
         """Test name attribute type"""
         new = Review()
-        new.name = "Kaicee"
+        new.name = "Kaice"
         self.assertEqual(type(new.name), str)
 
     def test_password(self):
